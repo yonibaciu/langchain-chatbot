80fb90d7e03828574b9172586d0e017f58d2dce4
Yoni Baciu <yoni.baciu@persado.com>
2024-02-07 20:57:56 -0500

    Added error handling when checking the flask service status

--- nextjs-app/app/components/flask_check.tsx
+++ nextjs-app/app/components/flask_check.tsx
@@ -1,22 +1,26 @@
-async function check() {
-  const res = await fetch('http://localhost:5001/check', { cache: 'no-store' })
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-}
-
 export default async function FlaskCheck() {
-  const data = await check()
+  async function check() {
+    try {
+      const res = await fetch('http://localhost:5001/check', { cache: 'no-store' })
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+      }
+     
+      const data = await res.json()
+      return data.status;
+    } catch (error) {
+      return 'NOT OK! Probaby the flask service is not running.';
+    }
+  }
 
-  console.log(data);  
+  const status = await check();
+
+  console.log(status);  
 
   return (
     <div className="flex items-center mb-6">
-      Flask server status: {data.status}
+      Flask server status: {status}
     </div>
   );
 }
