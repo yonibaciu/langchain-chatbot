6804e5faedfb1251cbdfc2e769525aec8300c203
Yoni Baciu <yoni.baciu@persado.com>
2024-02-16 12:40:46 -0500

    refactored API check

--- nextjs-app/app/actions.ts
+++ nextjs-app/app/actions.ts
@@ -30,3 +30,23 @@ export async function loadUrl(url: string) {
   console.log(data);
   return data;
 }
+
+export async function checkAPI() {
+  function timeout(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  try {
+    await timeout(1000); // forcing a delay to see Suspense working
+    const res = await fetch(`${FASTAPI_SERVER}/check`, { cache: 'no-store' })
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
+   
+    const data = await res.json()
+    return data.status;
+  } catch (error) {
+    return 'NOT OK! Probaby the FastAPI service is not running.';
+  }
+}
--- nextjs-app/app/components/fastapi_check.tsx
+++ nextjs-app/app/components/fastapi_check.tsx
@@ -1,25 +1,7 @@
+import { checkAPI } from "../actions";
+
 export default async function FlaskCheck() {
-  function timeout(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  async function check() {
-    try {
-      await timeout(1000); // forcing a delay to see Suspense working
-      const res = await fetch('http://localhost:5001/check', { cache: 'no-store' })
-      if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch data')
-      }
-     
-      const data = await res.json()
-      return data.status;
-    } catch (error) {
-      return 'NOT OK! Probaby the FastAPI service is not running.';
-    }
-  }
-
-  const status = await check();
+  const status = await checkAPI();
 
   console.log(status);  
 
